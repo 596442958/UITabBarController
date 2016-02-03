0a5980f60ededfd3b01a20ad8304afb6204466d7
Miles <596442958@qq.com>
2016-02-04 05:38:55 +0800

    Initial Commit

---
+++ UITabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITabBarController
+//
+//  Created by Miles on 2/4/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITabBarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITabBarController
+//
+//  Created by Miles on 2/4/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
